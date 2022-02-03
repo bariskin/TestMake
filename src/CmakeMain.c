64e77fc308a688cd064779993f14ef00e2ef75a8
bariskin <chiurcciu@mail.ru>
2022-02-03 15:17:59 +0300

    added git commands

--- src/CmakeMain.c
+++ src/CmakeMain.c
@@ -1,7 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+void printHelp(void)
+   {
+    printf("git command:\n"); 
+
+    printf(" git status\n");
+
+    printf(" echo build > .gitignore\n");
+
+    printf(" git add .   \n");
+
+    printf (" git commit -m   commitText\n");
+
+    printf(" git push\n");
+   }
+
 int main(int argc, char* argv[])
    {
     printf("Test Cmake");
+
+    printHelp();
    }
